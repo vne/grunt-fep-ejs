65fdcf133ee986ef9eeabd9ea83467dbed9897bb
louxiaojian <louxiaojian@gmail.com>
2015-03-18 17:47:30 +0800

    data增加options参数

--- tasks/ejs.js
+++ tasks/ejs.js
@@ -5,55 +5,56 @@
 
 'use strict';
 
-module.exports = function(grunt){
+module.exports = function(grunt) {
   var chalk = require('chalk'),
-  parseContent = function(content) {
-    return content;  
-  };
+    parseContent = function(content) {
+      return content;
+    };
 
-  grunt.registerMultiTask('ejs', 'Compile ejs templates.', function(){
-  
+  grunt.registerMultiTask('ejs', 'Compile ejs templates.', function() {
+
     var options = this.options({
       namespace: 'JST'
     });
-    var data = options.data,n = 0;
+    var data = options.data,
+      n = 0;
     delete options.data;
 
     var processContent = options.content || parseContent;
 
-    this.files.forEach(function(fileitem){
-    
+    this.files.forEach(function(fileitem) {
+
       var templates = [];
 
-      if(fileitem.src.length < 1){
+      if (fileitem.src.length < 1) {
         grunt.log.warn('File "' + chalk.red(fileitem.orig.src) + '" not found.');
         return false
       }
 
-      fileitem.src.forEach(function(filepath){
+      fileitem.src.forEach(function(filepath) {
 
-          var fileContent = processContent(grunt.file.read(filepath)),
-              compiled,
-              compileFn;
+        var fileContent = processContent(grunt.file.read(filepath)),
+          compiled,
+          compileFn;
 
-          options.filename = filepath;
+        options.filename = filepath;
 
-          try {
-          
-            var ejs = fileitem.orig.ejs = require('ejs');
-            
-            fileitem.orig.data = typeof data === 'function' ? data.call(fileitem.orig, fileitem.dest, fileitem.src) : data;
+        try {
 
-            compileFn = ejs.compile(fileContent, options);
-            compiled = options.client ? compileFn : compileFn(fileitem.orig.data);
+          var ejs = fileitem.orig.ejs = require('ejs');
 
-          }catch(e){
-            grunt.log.error(e);
-            grunt.fail.warn('ejs failed to compile "' + filepath + '".');
-            return false;
-          }
+          fileitem.orig.data = typeof data === 'function' ? data.call(fileitem.orig, fileitem.dest, fileitem.src, options) : data;
 
-          templates.push(compiled);
+          compileFn = ejs.compile(fileContent, options);
+          compiled = options.client ? compileFn : compileFn(fileitem.orig.data);
+
+        } catch (e) {
+          grunt.log.error(e);
+          grunt.fail.warn('ejs failed to compile "' + filepath + '".');
+          return false;
+        }
+
+        templates.push(compiled);
       });
 
       if (templates.length < 1) {
